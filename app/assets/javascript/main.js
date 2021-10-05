5dae5895809ea9036a315da772eba285c12de589
Jeannette Speight <JANSP@Mark-Hankins-MacBook-Pro.local>
2021-10-05 12:21:51 +0100

    A short message about the change your pushing

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,35 @@
-// ES6 or Vanilla JavaScript
+$(document).ready(function () {
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+	
+	$('ul.tabs1 li').click(function(){
+		var tab_id = $(this).attr('data-tab1');
+
+		$('ul.tabs1 li').removeClass('current1');
+		$('.tab1-content').removeClass('current1');
+
+		$(this).addClass('current1');
+		$("#"+tab_id).addClass('current1');
+         
+	})
+
+
+    $('.internal-table tr').click(function() {
+        var href = $(this).find("a").attr("href");
+        if(href) {
+            window.location = href;
+        }
+    });
+
+
+    
+});
