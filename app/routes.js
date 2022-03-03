72887d04fab58c873c0948c6e8afca08e6daedc1
Matthew Murphy <MATMU@MATMU-MacBook-Pro.local>
2022-03-03 11:09:06 +0000

    Character counter and error message when submitting

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 
-router.post('/app/views/note-pcn-info', (req, res) => {
+  router.post('/note-pcn/', (req, res) => {
     // Make a variable and give it the value from 'message'(text area)
     const charNumber = req.session.data['message'];
     let charLength = charNumber.length;
@@ -15,10 +15,10 @@ router.post('/app/views/note-pcn-info', (req, res) => {
     // Check whether the variable matches a condition
     if (charLength > maxLength) {
       // Send user to error page
-      res.redirect('/app/views/note-pcn-error');
+      res.redirect('/note-pcn-error');
     } else {
       // Send user to normal page
-      res.redirect('/app/views/css-pcn-view-note');
+      res.redirect('/css-pcn-view-note');
     }
   });
 
