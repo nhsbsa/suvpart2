3fa64855a68963573a82e4d43d2c1ecf42707387
Matthew Murphy <MATMU@MATMU-MacBook-Pro.local>
2022-03-03 10:34:56 +0000

    Character Count change

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,21 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+
+router.post('/app/views/note-pcn-info', (req, res) => {
+    // Make a variable and give it the value from 'message'(text area)
+    const charNumber = req.session.data['message'];
+    let charLength = charNumber.length;
+    const maxLength = 2048;
+  
+    // Check whether the variable matches a condition
+    if (charLength > maxLength) {
+      // Send user to error page
+      res.redirect('/app/views/note-pcn-error');
+    } else {
+      // Send user to normal page
+      res.redirect('/app/views/css-pcn-view-note');
+    }
+  });
+
 module.exports = router;
