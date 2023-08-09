11efab509ffd33e767fd861e4c73a71c78f37305
Iona Smith <iona.smith3@nhs.net>
2023-08-09 16:32:20 +0100

    edits to layout of DD pages

--- app/routes.js
+++ app/routes.js
@@ -72,4 +72,15 @@ const router = express.Router();
     }
 })
 
+// pay-pcn-extend.html in v3 folder - decide which page to direct to depending on the answer chosen
+router.post('v3-iona/pay-pcn-extend', function(request, response) {
+
+  var country = request.session.data['example-inline']
+  if (country == "yes"){
+      response.redirect("pcn-payment-card")
+  } else {
+      response.redirect("pay-pcn-DD-start")
+  }
+})
+
 module.exports = router;
