e225a3232bc0876f4f49482423f0682938decb9a
Jamie Bradford <jamie.bradford@nhsbsa.nhs.uk>
2026-03-09 15:50:41 +0000

    Prototype kit update
    Updating prototype kit to 8.02

--- app/config.js
+++ app/config.js
@@ -2,18 +2,8 @@
 
 module.exports = {
   // Service name
-  serviceName: 'NHS.UK prototype kit',
+  serviceName: 'Single User View',
 
-  // Port to run nodemon on locally
-  port: 2000,
-
-  // Automatically stores form data, and send to all views
-  useAutoStoreData: 'true',
-
-  // Enable cookie-based session store (persists on restart)
-  // Please note 4KB cookie limit per domain, cookies too large will silently be ignored
-  useCookieSessionStore: 'false',
-
-  // Enable or disable built-in docs and examples.
-  useDocumentation: true,
-};
+  // Port to run the prototype on locally
+  port: 3000
+}
--- app/filters.js
+++ app/filters.js
@@ -1,51 +1,8 @@
-module.exports = function (env) { /* eslint-disable-line no-unused-vars */
-  /**
-   * Instantiate object used to store the methods registered as a
-   * 'filter' (of the same name) within nunjucks. You can override
-   * gov.uk core filters by creating filter methods of the same name.
-   * @type {Object}
-   */
-  const filters = {};
-
-
-  //
-  // ALTER DATE BY NUMBER OF MONTHS FUNCTION
-  //
-  filters.alterTodaysDateByNumberOfMonths = function( monthOffset, daysOffset ){
-
-    daysOffset = ( typeof daysOffset === 'number' && parseInt(daysOffset) ) ? parseInt(daysOffset) : 0;
-
-    let today = new Date();
-    today.setDate(today.getDate() + daysOffset);
-    var d = today.getDate();
-
-    today.setMonth(today.getMonth() + monthOffset);
-    if (today.getDate() !== d ) {
-      today.setDate(0);
-    }
-
-    return today.toLocaleDateString('en-GB', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    });
-
-  };
-
-   //
-  // ALTER DATE BY NUMBER OF DAYS FUNCTION
-  //
-  filters.alterTodaysDateByNumberOfDays = function( daysOffset, format ){
-
-    let today = new Date();
-    today.setDate(today.getDate() + daysOffset);
-
-    let formatObj = ( format === 'long' ) ? { day: 'numeric', month: 'long', year: 'numeric' } : { month: '2-digit',day: '2-digit',year: 'numeric' };
-
-    // Manually format the date to avoid leading zeros (day, month, year)
-    return today.toLocaleDateString('en-GB', formatObj );
-
-  };
+/**
+ * @param {Environment} env
+ */
+module.exports = function (env) {
+  const filters = {}
 
   /* ------------------------------------------------------------------
     add your methods to the filters obj below this comment block:
@@ -78,8 +35,10 @@ module.exports = function (env) { /* eslint-disable-line no-unused-vars */
 
   ------------------------------------------------------------------ */
 
-  /* ------------------------------------------------------------------
-    keep the following line to return your filters to the app
-  ------------------------------------------------------------------ */
-  return filters;
-};
+  /* keep the following line to return your filters to the app  */
+  return filters
+}
+
+/**
+ * @import { Environment } from 'nunjucks'
+ */
